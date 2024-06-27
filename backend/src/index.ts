c1e8b39b9c0b02bba3fcfa281c5203fddec41bc9
MihirKotecha <mihir.k1@ahduni.edu.in>
2024-06-27 13:23:37 +0530

    Remove DB URL

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,34 +1,44 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    prisma : PrismaClient;
+  };
+}>();
 
-
-app.post('/api/v1/user/signup', (c) => {
-  return c.text('Hello World!!');
+app.use((c, next): any => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate()); 
+  next();
 });
 
-app.post('/api/v1/user/signin', (c) => {
-  return c.text('Hello World!!');
+app.post("/api/v1/user/signup", (c) => {
+
+  return c.text("Hello World!!");
 });
 
-app.post('/api/v1/blog', (c) => {
-  return c.text('Hello World!!');
+app.post("/api/v1/user/signin", (c) => {
+  return c.text("Hello World!!");
 });
 
-app.put('/api/v1/blog', (c) => {
-  return c.text('Hello World!!');
+app.post("/api/v1/blog", (c) => {
+  return c.text("Hello World!!");
 });
 
-app.get('/api/v1/blog/:id', (c) => {
-  return c.text('Hello World!!');
+app.put("/api/v1/blog", (c) => {
+  return c.text("Hello World!!");
 });
 
-app.get('/api/v1/blog/bulk', (c) => {
-  return c.text('Hello World!!');
+app.get("/api/v1/blog/:id", (c) => {
+  return c.text("Hello World!!");
 });
 
-export default app
+app.get("/api/v1/blog/bulk", (c) => {
+  return c.text("Hello World!!");
+});
 
-
-//DIRECT_URL="postgresql://meduimDb_owner:nwM30lHajYQX@ep-shy-darkness-a1uwpuo6.ap-southeast-1.aws.neon.tech/meduimDb?sslmode=require"
-//DATABASE_URL="prisma://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiZmVhOWQxYzYtNjllNi00MDU5LWIzOGEtY2NkNjI3NTA0MDBiIiwidGVuYW50X2lkIjoiNTZmNTc4NTU4OTMyYjc2Yzc4OWJiZGRkOTc2YzNlNTM5Y2I1OTcyN2UxYzg5NmFhY2UzNDE2Y2ZiNDgxMzlmNiIsImludGVybmFsX3NlY3JldCI6IjhhMGIzZDc1LWE3OTMtNGViMS04NjU2LTQwODY4MDY0MTNkZSJ9.gQBGPgfkQGtPTvNnb7NXr7OfQRI3ZEqypp7aXbTBvm0"
+export default app;
