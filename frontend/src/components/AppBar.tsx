92b91b57f4df09b5a85115e0bbc9ccfae6c1b07c
MihirKotecha <mihir.k1@ahduni.edu.in>
2024-08-22 11:24:50 +0530

    Made author avtar and symbol navigate to user and home page respectively.

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -7,12 +7,18 @@ const AppBar = () => {
   const handleClick = () => {
     navigate("/blogpost")
   }
+
+  const handleUserPage = () => {
+    navigate("/user")
+  }
   return (
     <div className="flex justify-between px-12 pb-6 shadow-lg items-center">
-      <div className="font-bold text-lg">BlogIt</div>
+      <div className="font-bold text-lg cursor-pointer" onClick={()=>{navigate("/blogs")}}>BlogIt</div>
       <div className="flex justify-center items-center">
         <button className="mr-10 w-20 h-8 rounded-2xl text-lg text-black bg-green-500" onClick={handleClick}>Post</button>
-        <AuthorAvatar authorName="Mihir" />
+        <div onClick={handleUserPage} className="cursor-pointer">
+          <AuthorAvatar authorName="Mihir" />
+        </div>
       </div>
 
     </div>
